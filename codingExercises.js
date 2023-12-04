b49b1bbb427117f366994b99fd4d804c4bcef29a
Seth Bruner <bruner.seth@outlook.com>
2023-12-04 16:19:44 -0500

    solutions to string/array iterations

--- codingExercises.js
+++ codingExercises.js
@@ -20,17 +20,90 @@ Object.freeze(bonusObj2);
 
 
 /** for loop - function name: forSolution */
+let forSolution = (str) => {
+
+  let newString = "";
+
+  for (let index = 0; index < str.length; index++) {
+
+    let letterIndex = str.indexOf('letter') + 5;
+
+    if (str[index] === 'e' && index > letterIndex) {
+      continue;
+    } else {
+      newString += str[index];
+    }
+  }
+
+  return newString;
+};
+
+//console.log(forSolution(obj.text));
 
 
 /** while loop - function name: whileSolution */
+let whileSolution = (str) => {
+  let count = 0;
+  let newString = "";
+  let letterIndex = str.indexOf('letter') + 5;
 
+  while (count < str.length) {
+
+    if (str[count] === 'e' && count > letterIndex) {
+      count++;
+      continue;
+    } else {
+      newString += str[count];
+    }
+    count++;
+  }
+
+  return newString;
+};
+
+//console.log(whileSolution(obj.text));
 
 /** do while loop - function name: doWhileSolution */
+let doWhileSolution = (str) => {
 
+  let newString = "";
+  let letterIndex = str.indexOf('letter') + 5;
+  let tracker = 0;
+
+  do {
+    if (str[tracker] === 'e' && tracker > letterIndex) {
+      newString[tracker] = "";
+    } else {
+      newString += str[tracker];
+    }
+
+    tracker++;
+  } while (tracker < str.length);
+
+  return newString;
+};
+//console.log(doWhileSolution(obj.text));
 
 /** forEach iteration method  - function name: forEachSolution */
 
+let forEachSolution = (str) => {
+  let newString = "";
+  let letterIndex = str.indexOf('letter') + 5;
+  let index = 0;
 
+  str.split("").forEach((letter) => {
+    if (letter === 'e' && index > letterIndex) {
+      newString += "";
+    } else {
+      newString += letter;
+    }
+    index++;
+  });
+
+  return newString;
+};
+
+console.log(forEachSolution(obj.text));
 /** filter iteration method  - function name: 'filterSolution' */
 
 
